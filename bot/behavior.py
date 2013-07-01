0f13cf554b3085e4b133882e0fef03022cb99e40
Mohammad Adil <mmadil_14@yahoo.com>
2013-07-01 23:33:57 +0530

    need to get chapters now

--- bot/behavior.py
+++ bot/behavior.py
@@ -21,24 +21,31 @@ def regexify(data):
         return ''
 
 
+
 def get_nick(string):
     return string[0:string.index('!')]
 
-def get_module(string):
-    print string[string.index('!') + 6:]
-    return string[string.index('!') + 6:]
+
 
 def list_modules():
     string = ' '
 
     if len(MODULES) == 1:
-        return 'We have 1 module : ' + MODULES[0]
+        return 'We have 1 module : ' + str(MODULES[0])
     else:
         count = 1
         for e in MODULES:
             string += str(e) + ', '
-        return 'We have '+str(len(MODULES))+' modules : '+ string[:-2]
-	
+        return 'We have '+str(len(MODULES))+' modules : '+ str(string[:-2])
+
+
+
+def get_module(string):
+    return string[string.index('!') + 6:][:-1]
+
+
+def get_chapters(string):
+    pass
 
 
 # AI Functions
@@ -50,13 +57,13 @@ def ai(data, bot, irc):
         irc.send('PONG ' + data.split() [1] + '\r\n')
 
     if data.find('Message of the Day') != -1:
-        irc.send('JOIN ' + channel + '\r\n')
+        irc.send('JOIN ' + str(channel) + '\r\n')
 
     if data.find('+iwR') != -1:
         irc.send('PRIVMSG NickServ IDENTIFY ' + str(nick) + ' ' + str(password) + '\r\n')
 
     if data.find('KICK') != -1:
-        irc.send('JOIN ' + channel + '\r\n')
+        irc.send('JOIN ' + str(channel) + '\r\n')
 
     if bot_type == 'Helper':
         regexed_list = []
@@ -65,17 +72,17 @@ def ai(data, bot, irc):
             regexed_list = regexify(data)
             msgto = get_nick(regexed_list[2])
             string = list_modules(regexed_list[6])
-            irc.send('PRIVMSG ' + msgto + ' :Hi ' + msgto +' ! \r\n')
-            irc.send('PRIVMSG ' + msgto + ' :What would you like to learn ?\r\n')
-            irc.send('PRIVMSG ' + msgto + ' :You can learn from any of them : \r\n')
-            irc.send('PRIVMSG ' + msgto + ' :'+ string + '\r\n')
-            irc.send('PRIVMSG ' + msgto + ' :List the chapters using !list <modulename>\r\n')
+            irc.send('PRIVMSG ' + str(msgto) + ' :Hi ' + msgto +' ! \r\n')
+            irc.send('PRIVMSG ' + str(msgto) + ' :What would you like to learn ?\r\n')
+            irc.send('PRIVMSG ' + str(msgto) + ' :You can learn from any of them : \r\n')
+            irc.send('PRIVMSG ' + str(msgto) + ' :'+ string + '\r\n')
+            irc.send('PRIVMSG ' + str(msgto) + ' :List the chapters using !list <modulename>\r\n')
 
         if data.find('PRIVMSG '+ nick + ' :!list') != -1:
             regexed_list = regexify(data)
             msgto = get_nick(regexed_list[2])
             module = get_module(regexed_list[6])
-            irc.send('PRIVMSG ' + msgto + ' :Module ' + module +' has the following chapters :\r\n')
+            irc.send('PRIVMSG ' + str(msgto) + ' :Module '+ str(module) +' has the following chapters : \r\n')
 
 
     if bot_type == 'Teacher':
